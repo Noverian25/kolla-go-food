b08729a451ce8db13771c9f3ff15d0a2df7fb0df
@Noverian25 <noverian45@gmail.com>
2018-09-01 11:44:32 +0700

    Initial commit

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,52 +1,31 @@
 require 'rails_helper'
 
 describe Food do
+
   it "is valid with a name and description" do
-  	food = Food.new(
-      name: "Nasi Uduk",
-      description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
-      price: 10000.0
-    )
-    expect(food).to be_valid
+    expect(FactoryGirl.build(:food)).to be_valid
   end
-  
+
   it "is invalid without a name" do
-    food = Food.new(
-      name: nil,
-      description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
-      price: 10000.0
-    )
+    food = FactoryGirl.build(:food, name: nil)
     food.valid?
     expect(food.errors[:name]).to include("can't be blank")
   end
 
   it "is invalid without a description" do
-    food = Food.new(
-      name: "Nasi Uduk",
-      description: nil,
-      price: 10000.0
-    )
+    food = FactoryGirl.build(:food, description: nil)
     food.valid?
     expect(food.errors[:description]).to include("can't be blank")
   end
-
+  
   it "is invalid with a duplicate name" do
-    food1 = Food.create(
-      name: "Nasi Uduk",
-      description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
-      price: 10000.0
-    )
-    
-    food2 = Food.new(
-      name: "Nasi Uduk",
-      description: "Just with a different description.",
-      price: 10000.0
-    )
+    food1 = FactoryGirl.create(:food, name: "Nasi Uduk")
+    food2 = FactoryGirl.build(:food, name: "Nasi Uduk")
 
     food2.valid?
     expect(food2.errors[:name]).to include("has already been taken")
   end
-
+ 
   it "returns a sorted array of results that match" do
     food1 = Food.create(
       name: "Nasi Uduk",
@@ -91,6 +70,58 @@ describe Food do
     expect(Food.by_letter("N")).not_to include(food2)
   end
 
+  it "is valid with numeric price greater or equal to 0.01" do
+    food = Food.new(
+      name: "Nasi Uduk",
+      description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
+      price: 0.01
+    )
+
+    expect(food).to be_valid
+  end
+
+  it "is invalid with non numeric price" do
+    food = Food.new(
+      name: "Nasi Uduk",
+      description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
+      price: "sepuluh ribu"
+    )
+
+    expect(food.errors[:price]).to include("is not a number")
+  end
+
+  it "is invalid with non numeric price" do
+    food = Food.new(
+      name: "Nasi Uduk",
+      description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
+      price: -1000
+    )
+
+    expect(food.errors[:price]).to include("must be greater than or equal to 0.01")
+  end
+
+  it "is valid with image_url ending with '.gif', '.jpg', or '.png'" do
+    food = Food.new(
+      name: "Nasi Uduk",
+      description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
+      image_url: "Nasi Uduk.jpg",
+      price: 0.01
+    )
+
+    expect(food).to be_valid
+  end
+
+  it "is invalid with image_url ending not with '.gif', '.jpg', or '.png'" do
+    food = Food.new(
+      name: "Nasi Uduk",
+      description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
+      image_url: "Nasi Uduk.csv",
+      price: 0.01
+    )
+
+    expect(food.errors[:image_url]).to include("must be a URL for GIF, JPG or PNG image.")
+  end
+
   describe "filter name by letter" do
     before :each do
       @food1 = Food.create(
@@ -123,6 +154,32 @@ describe Food do
         expect(Food.by_letter("N")).not_to include(@food2)
       end
     end
+  end
+
+  describe "invalid without name or description" do
+    before :each do
+      @food = Food.new(
+        name: nil,
+        description: nil,
+        price: 10000.0
+      )
+    end
+    
+    it "is invalid without name" do
+      # food = build(:food, name: nil)
+      @food.valid?
+      expect(@food.errors[:name]).to include("can't be blank")
+    end
+
+    it "is invalid without description" do
+      # food = build(:food, description: nil)
+      @food.valid?
+      expect(@food.errors[:description]).to include("can't be blank")
+    end
+  end
+
+  it "has a valid factory" do
+    expect(FactoryGirl.build(:food)).to be_valid
   end
 
 end
